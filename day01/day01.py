9585335f1e10b3905ce3ac837529201e217d1450
lisyce <lisyce@gmail.com>
2024-11-30 16:26:37 -0800

    setup!

---
+++ day01/day01.py
@@ -0,0 +1,24 @@
+import sys
+
+def part_one(f) -> int:
+  pass
+
+def part_two(f) -> int:
+  pass
+
+if __name__ == "__main__":  
+  args = sys.argv
+  file_name = "input_test.txt" if "--test" in args else "input.txt"
+  f = open(file_name)
+  
+  if "1" not in args and "2" not in args:
+    print("Part One:", part_one(f))
+    print("Part Two:", part_two(f))
+  else:
+    part = int(args[1])
+    if part == 1:
+      print("Part One:", part_one(f))
+    else:
+      print("Part Two:", part_two(f))
+    
+  f.close()
